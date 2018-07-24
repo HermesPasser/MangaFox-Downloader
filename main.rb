6c35f6d2c0cb2f9f64ac51510d6e6ad914ac4075
Hermes Passer <dsldouglas@hotmail.com>
2018-07-24 20:25:44 -0300

    domain url changed and default location to download

--- main.rb
+++ main.rb
@@ -1,17 +1,32 @@
 #By Hermes Passer in 11/06/2017
 require 'thread'
+require 'fileutils'
 load 'external/updatewp.rb'
 require_relative 'mdownloader/mfdownloader.rb'
 
 include MDownloader
 include Hermes::Update
+
+# --------------------------------------------------- #
+# If MangaFox change it domain url then update this.
+# --------------------------------------------------- #
+$SITE_URL = 'fanfox.net'
+# --------------------------------------------------- #
+
 #aparentemente as msgs não aparecem no log da gui
 
 #debugar e ver por que o mesmo metodo esta sendo chamada varias vezes para o mesmo 
 #link, mesmo que não tenham links repetidos e ele execute em um for sem change para repetir
-$VERSION = "0.5"; $LOG = ""
-manga = vol = chap = path = ""
+$VERSION = '0.6'; $LOG = '';
+manga = vol = chap = path = ''
 dont_execute = false
+
+def create_manga_folder(dirname)
+	unless File.exist?("#{Dir.pwd}/#{dirname}")
+		FileUtils.mkdir_p("#{Dir.pwd}/#{dirname}")
+	end
+	return "#{Dir.pwd}/#{dirname}"
+end
 
 def update
 	up = Hermes::Update::UpdateByWebPage.new("mfdownloader", $VERSION, "gladiocitrico.blogspot.com.br/p/update.html")
@@ -25,7 +40,7 @@ end
 
 def printlogo
 	puts "\t    MangaFox Downloader #{$VERSION} Command Line"
-	puts "\tby Hermes Passer (gladiocitrico.blogspot.com)"
+	puts "\tby Hermes Passer (hermespasser.github.io)"
 end
 
 def printhelp
@@ -61,9 +76,13 @@ if manga == "" && chap == "" && path == ""
 		end
 	end
 else
-	if MDownloader::Mangafox.url_page_exits?("mangafox.me", "/manga/#{manga}/")
+	if MDownloader::Mangafox.url_page_exits?($SITE_URL, "/manga/#{manga}/")
+		if path == '' # If none path is set then create one in current program folder
+			path = create_manga_folder("manga/#{manga}/#{vol}/#{chap}")
+		end
+
 		mfd = Mangafox.new(path, manga, vol, chap)
 		mfd.download_chapter
-	else puts "\"mangafox.me/manga/#{manga}/\" cannot be found"
+	else puts "\"#{$SITE_URL}/manga/#{manga}/\" cannot be found"
 	end
 end
